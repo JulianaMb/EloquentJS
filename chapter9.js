4af1dc644faed45bd94c3f23b245e9b5946b5be1
Juliana <julianamb.analista@gmail.com>
2019-05-07 08:16:19 -0400

    update

--- chapter9.js
+++ chapter9.js
@@ -24,7 +24,7 @@ verify(/\w{7}/,
        ["hottentottententen"],
        ["no", "hotten totten tenten"]);
 
-verify(/\b[^eE]\b/,
+verify(/\b[^\We]+\b/i,
        ["red platypus", "wobbling nest"],
        ["earth bed", "learning ape", "BEET"]);
 
@@ -44,3 +44,21 @@ let text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
 console.log(text.replace(/(\W)'|'(\W)|^'/g, "$1\"$2"));
 // → "I'm the cook," he said, "it's my job."
+
+
+// Fill in this regular expression.
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
